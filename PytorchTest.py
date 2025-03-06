228323a47fe5240b0b065918957db44bea08d21a
li <2043911504@qq.com>
2025-03-06 18:10:02 +0800

    1

--- PytorchTest.py
+++ PytorchTest.py
@@ -1,3 +1,18 @@
+import copy
+import numpy as np
+import torch.nn as nn
 import torch
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import math
+from torch.autograd import Variable
+import torch.nn.functional as F
+import os
 
-print(torch.__version__)
+torch.manual_seed(42)
+file_name = "test.txt"
+
+with open(file_name, "r", encoding='utf-8') as f:
+    text = f.read()
+
+print(text)
